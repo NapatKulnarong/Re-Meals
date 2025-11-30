34e5f4c313528ee250a2adbb0d668d4f092017d3
karnponpoochitkanon <karnpon.p@ku.th>
2025-11-30 14:51:13 +0700

    feat: Add Donations model, API

--- backend/restaurants/urls.py
+++ backend/restaurants/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import RestaurantViewSet
+from .views import RestaurantViewSet, DonationViewSet
 
 router = DefaultRouter()
 router.register("restaurants", RestaurantViewSet, basename="restaurants")
+router.register("donations", DonationViewSet)
 
 urlpatterns = router.urls
