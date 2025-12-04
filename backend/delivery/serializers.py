38dcf3a8edc6baf8a646a0238ae52d47e4179539
Nisara Ploysuttipol <nisara.pl@ku.th>
2025-12-04 17:13:48 +0700

    feat: add Delivery app with API routes and update admin role registrations - Created delivery app with Delivery model, serializer, viewset, and router - Added delivery to INSTALLED_APPS and registered API path in re_meals_api/urls.py - Implemented DeliveryAdmin with useful list, filter, and search fields - Updated admin pages for related user roles - Ensured all delivery-related relationships are visible in admin

---
+++ backend/delivery/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+
+from .models import Delivery
+from users.models import User
+from warehouse.models import Warehouse
+from community.models import Community
+from donation.models import Donation
+        
+class DeliverySerializer(serializers.ModelSerializer):
+    user_id = serializers.SlugRelatedField(
+        source="user_id",
+        slug_field="user_id",
+        queryset=User.objects.all(),
+        allow_null=True,
+        required=False
+    )
+    warehouse_id = serializers.SlugRelatedField(
+        source="warehouse_id",
+        slug_field="warehouse_id",
+        queryset=Warehouse.objects.all(),
+        allow_null=True,
+        required=False
+    )
+    community_id = serializers.SlugRelatedField(
+        source="community_id",
+        slug_field="community_id",
+        queryset=Community.objects.all(),
+        allow_null=True,
+        required=False
+    )
+    donation_id = serializers.SlugRelatedField(
+        source="donation_id",
+        slug_field="donation_id",
+        queryset=Donation.objects.all(),
+        allow_null=True,
+        required=False,
+    )
+    
+    class Meta:
+        model = Delivery
+        fields = [
+            "delivery_id",
+            "delivery_type",
+            "pickup_time",
+            "dropoff_time",
+            "pickup_location_type",
+            "dropoff_location_type",
+            "warehouse_id",
+            "user_id",
+            "donation_id",
+            "community_id",
+        ]
