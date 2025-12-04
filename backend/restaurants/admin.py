1cd3d51faf444e4c9fab7171124e8a994fe26b75
Nisara Ploysuttipol <nisara.pl@ku.th>
2025-12-04 14:44:41 +0700

    chore: register User, Donor, DeliveryStaff, Recipient and Restaurant models in Django admin - Added admin registrations for User, Donor, DeliveryStaff, Recipient, and Admin models in users/admin.py - Added Restaurant registrations in restaurants/admin.py - Added list_display and search_fields to improve admin usability - Verified Django admin is enabled in INSTALLED_APPS and admin URL is active

--- backend/restaurants/admin.py
+++ backend/restaurants/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Restaurant
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("restaurant_id", "name", "branch_name", "is_chain", "chain")
+    search_fields = ("restaurant_id", "name", "branch_name")
+    list_filter = ("is_chain",)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Donor, DeliveryStaff, Recipient, Admin as UserAdminModel
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "username",
-        "email",
-        "fname",
-        "lname",
-        "phone",
-        "bod",
-    )
+    list_display = ("username", "email", "fname", "lname", "phone", "bod")
     search_fields = ("username", "email")
+    ordering = ("user_id",)
+
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ("user", "restaurant_id")
+    search_fields = ("user__username", "user__fname", "user__lname", "restaurant_id")
+
+@admin.register(Recipient)
+class Recipient(admin.ModelAdmin):
+    list_display = ("user", "address", "community_id")
+    search_fields = ("user__fname", "user__lname", "community_id")
+
+admin.site.register(DeliveryStaff)
+admin.site.register(UserAdminModel)
